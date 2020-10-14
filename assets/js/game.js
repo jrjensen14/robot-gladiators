38267b569eef544eb9817db705b7844684bcf1de
JRJ <jjsocc171@msn.com>
2020-10-14 17:42:46 -0500

    console.log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("This is an alart! JavaScript is running!")
+var playerName = window.prompt("Whats is your robots's name");
+// WHAT IS THIS 
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Oour robot's name is " + playerNamegit)
+
+// this creates a function named "fight" //
+
+function fight() {
+window.alert("The fight has begun!")
+}
+// fight()
